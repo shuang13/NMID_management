c39bf6a0e42c78c1bf332baddb4523f1459c85e7
your nameshuang13 <794861654@qq.com>
2017-01-19 18:00:56 +0800

    富文本编辑器导入

---
+++ js/blog-upload.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    var ue = UE.getEditor('container');
+    //对编辑器的操作最好在编辑器ready之后再做
+    // ue.ready(function() {
+    //     $submit.on('click', function (event) {
+    //         //获取html内容，返回: 
+    //         var content = ue.getContent();
+        
+    //         var article = {
+    //             id: $.trim(articleId),
+    //             title: $.trim($title.val()),
+    //             content: $.trim(content),
+    //             tag: $.trim($tag.val()),
+    //         };
+    
+    //         $.ajax({
+    //             type: "POST",
+    //             url: "##",
+    //             data: article,
+    //             success: function(data){
+                
+    //             alert("发布成功！")
+                
+    //             }
+    //         })
+    //     })
+    // })
+})
