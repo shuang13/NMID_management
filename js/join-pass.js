843d05d9e3986e0e6f11d4a539c5ac25803757c3
zhanlinhan <1404136653@qq.com>
2017-02-10 16:40:02 +0800

    加入我们页面-js完善

--- js/join-pass.js
+++ js/join-pass.js
@@ -18,30 +18,10 @@ $(document).ready(function() {
                 "<div class=\"applicant-content\">"+
                     "<span class=\"content-text\">"+ '申请内容' + "</span>"+
                 "</div>"+
-                "<div class=\"join-btn\">"+
-                    "<div class=\"btn-not-pass\">"+
-                        "<a href=\"#\">不通过</a>"+
-                    "</div>"+
-                    "<div class=\"btn-pass\">"+
-                       "<a href=\"#\">通过</a>"+
-                    "</div>"+
-               "</div>"+
             "</div>")
             }
             main.append(frag);
         }
-    });
-	// 按钮
-	// 不通过
-	$('.btn-not-pass').on('click', function(event) {
-        event.preventDefault();
-        $.notice('加入我们提示：', '您已拒绝该用户的加入申请！', undefined, 300, 150);
-    });
-
-	// 通过
-    $('.btn-pass').on('click', function(event) {
-        event.preventDefault();
-        $.notice('加入我们提示：', '您已同意该用户的加入申请！', undefined, 300, 150);
     });
 
 })
---
+++ js/setting-user-list.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$.ajax({
+		type: "POST",
+		url: "",
+		data: {},
+		success: function (data) {
+			var data = JSON.parse(data);
+		}
+	});
+})
