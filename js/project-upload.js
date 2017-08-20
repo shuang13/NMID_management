35714d0d8b0bbd01fc0992f93459d187bdcdc902
zhanlinhan <1404136653@qq.com>
2017-08-20 11:13:30 +0800

    项目上传，未全部对接完成

--- js/project-upload.js
+++ js/project-upload.js
@@ -15,31 +15,34 @@ $(document).ready(function () {
     var $file_upload = $('#app-upload');
     var $link = $('#app-link');
     var $submit = $('.btn-submit');
-
-
+    // 获取当前10位时间戳
+    var time = Math.round(new Date().getTime()/1000);
     $submit.on('click', function (event) {
         event.preventDefault();
         // 每次点击按钮时，读取用户名和密码
         var info = {
+            // 必须参数
             // member: $member.val(),
             name: $name.val(),
             // size: $size.val(),
             version: $version.val(),
-            time: new Date().getTime()/1000,
-            support: $system.val(),
+            time: time,
             profile: $intro.val(),
             utility: $func.val(),
             feature: $feature.val(),
+            links: $link.val(),
+            // 可选参数
+            support: $system.val(),
             instruction: $instruction.val(),
-            links: $link.val()
+            
         };
 
         // 检测信息是否为空
-        $.each(info,function(index,item) {
-            if(info[index] == '') {
-                $.notice('项目更新提示：', '部分信息未填写！', undefined, 300, 150);  
-            }
-        })
+        // $.each(info,function(index,item) {
+        //     if(info[index] == '') {
+        //         $.notice('项目更新提示：', '部分信息未填写！', undefined, 300, 150);  
+        //     }
+        // })
         
         // 文件上传
         // $.ajaxFileUpload ({
